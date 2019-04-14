4b49ab64ef34fc6c378bcccf94c9fe91939c0c3c
Олег Семенов <sato.rc@yandex.ru>
2019-04-14 21:08:47 +0300

    Revert "Extract tooltip update functions"
    This reverts commit bfdd38a3d55dd3996e0cf193bd4d6de79de69fce.

--- src/lovely-chart/Tooltip.js
+++ src/lovely-chart/Tooltip.js
@@ -189,8 +189,13 @@ export function createTooltip(container, data, plotSize, palette, onSelectLabel)
   function _updateBalloon(statistics, xPx, labelIndex) {
     const label = _data.xLabels[labelIndex];
     const date = new Date(label.value);
-    _drawBaloonTitle(date, label);
-    _drawBalloonContent(statistics);
+    _balloon.children[0].innerHTML = `${WEEK_DAYS[date.getDay()]}, ${label.text}`;
+    _balloon.children[1].innerHTML = statistics.map(({ name, colorName, value }) => (
+      `<div class="dataset transition-container">
+        <span>${name}</span>
+        <span class="value transition top right ${colorName}">${formatInteger(value)}</span>
+      </div>`
+    )).join('');
 
     const left = Math.max(
       BALLOON_OFFSET + BALLOON_SHADOW_WIDTH,
@@ -198,29 +203,6 @@ export function createTooltip(container, data, plotSize, palette, onSelectLabel)
     );
     _balloon.style.left = `${left}px`;
     _balloon.classList.add('shown');
-  }
-
-  function _drawBaloonTitle(date, label) {
-    if (!_balloon.children[0].innerHTML) {
-      _balloon.children[0].innerHTML = `${WEEK_DAYS[date.getDay()]}, ${label.text}`;
-      return;
-    }
-
-    // TODO: update individual datasets
-  }
-
-  function _drawBalloonContent() {
-    if (!_balloon.children[1].innerHTML) {
-      _balloon.children[1].innerHTML = statistics.map(({ name, colorName, value }) => (
-        `<div class="dataset">
-          <span>${name}</span>
-          <span class="value right ${colorName}">${formatInteger(value)}</span>
-        </div>`
-      )).join('');
-      return;
-    }
-
-    // TODO: pdate individual datasets
   }
 
   function _hideBalloon() {
